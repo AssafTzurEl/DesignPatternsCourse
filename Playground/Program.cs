0ec40cf419db3cdfe9cb21ca36d503d9d748b424
Assaf Tzur-El <assaf@tzurel.co.il>
2019-01-30 15:01:50 +0200

    Add abstract factory projects

---
+++ Playground/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Random experiments in code.
+/// </summary>
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
