a8434c4ff14072ad766ad728de7c7ed1f3fc346a
Assaf Tzur-El <assaf.tzurel@gmail.com>
2019-05-20 09:34:29 +0300

    Fix Adapter example

--- Adapter/Adapter.cs
+++ Adapter/Adapter.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Adapter.v1
+namespace Adapter
 {
     /// <summary>
     /// Represents a tree of nodes, each one may contain other nodes (branches)
--- Adapter/Properties/AssemblyInfo.cs
+++ Adapter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Adapter.v2")]
+[assembly: AssemblyTitle("Adapter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Adapter.v2")]
+[assembly: AssemblyProduct("Adapter")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d72940c2-e216-4a73-80c9-e89d50badd47")]
+[assembly: Guid("bc7fa8b1-2add-401d-8bd4-a22fe484363d")]
 
 // Version information for an assembly consists of the following four values:
 //
